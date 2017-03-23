2d322f137ee1cdb5000b5d24330be5803479f5fb
kekoa428 <thomaskekoafarr@gmail.com>
2017-03-23 14:26:45 -0700

    complete section 5.17 with implementing google map

--- Part3/ReduxSimpleStarter/src/components/google_map.js
+++ Part3/ReduxSimpleStarter/src/components/google_map.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 class GoogleMap extends Component {
+  // lifecyle method
   componentDidMount() {
     new google.maps.Map(this.refs.map, {
       zoom: 12,
