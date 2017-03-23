b14da3b4c581462b29a5e9858ae8076101e5d1ed
kekoa428 <thomaskekoafarr@gmail.com>
2017-03-23 13:57:09 -0700

    complete section 5.14 making reusable charts

---
+++ Part3/ReduxSimpleStarter/src/components/chart.js
@@ -0,0 +1,15 @@
+// class vs function based component
+// state not changing (just data coming in)
+
+import React from 'react';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
+
+export default (props) => {
+  return(
+    <div>
+      <Sparklines height={120} width={180} data={props.data}>
+        <SparklinesLine color={props.color} />
+      </Sparklines>
+    </div>
+  );
+}
