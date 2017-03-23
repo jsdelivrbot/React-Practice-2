274e76f2bae467e3b8ba190f21459ad5e5d58a6d
kekoa428 <thomaskekoafarr@gmail.com>
2017-03-23 13:29:42 -0700

    complete section 5.11

--- Part3/ReduxSimpleStarter/src/reducers/reducer_weather.js
+++ Part3/ReduxSimpleStarter/src/reducers/reducer_weather.js
@@ -1,10 +1,10 @@
 import { FETCH_WEATHER } from '../actions/index';
 
 export default function(state = [], action) {
-  swtich(action.type) {
+  switch(action.type) {
   case FETCH_WEATHER:
-    // return state.concat([action.payload.data]);  // not mutating state - returning a new instance of state
-    return [ action.payload.data, ...state ]; //... take entries inside of this array and put in other array
+    return state.concat([action.payload.data]);  // not mutating state - returning a new instance of state
+    // return [ action.payload.data, ...state ]; //... take entries inside of this array and put in other array
   }
 
   return state;
