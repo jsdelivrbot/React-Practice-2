24a826fbd2d7ecaf2b7306481828a85d301077ca
kekoa428 <thomaskekoafarr@gmail.com>
2017-03-23 14:18:26 -0700

    complete section 5.16 google maps

---
+++ Part3/ReduxSimpleStarter/src/components/google_map.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class GoogleMap extends Component {
+  componentDidMount() {
+    new google.maps.Map(this.refs.map, {
+      zoom: 12,
+      center: {
+        lat: this.props.lat,
+        lng: this.props.lon
+      }
+    });
+  }
+
+  render() {
+    // this.refs.map
+    return <div ref="map" />; //ref system allows us to get reference to an html element that is rendered to page
+  }
+}
+
+export default GoogleMap;
