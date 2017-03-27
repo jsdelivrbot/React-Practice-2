cba78b88c4899154ee71d36bb1d394a9a54cc771
kekoa428 <thomaskekoafarr@gmail.com>
2017-03-27 16:37:39 -0700

    change component to container in section 6 lecture 81

--- Part4 - blog post/src/components/posts_index.js
+++ Part4 - blog post/src/components/posts_index.js
@@ -1,5 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/index';
 
-export default () => {
-  return <div>List of blog posts</div>;
-};
+class PostsIndex extends Component {
+  componentWillMount() { // this is a lifecyle method (only called on the first render of dom)
+    // place for action to intially fetch data
+    this.props.fetchPosts();
+  }
+
+  render() {
+    return(
+      <div> List of blog posts</div>
+    );
+  }
+}
+
+export default connect(null, { fetchPosts })(PostsIndex);
+
+/*
+
+Other way to code it up*****
+
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchPosts } from '../actions/index';
+
+class PostsIndex extends Component {
+  componentWillMount() { // this is a lifecyle method (only called on the first render of dom)
+    // place for action to intially fetch data
+    this.props.fetchPosts();
+  }
+
+  render() {
+    return(
+      <div> List of blog posts</div>
+    );
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchPosts }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(PostsIndex);
+*/
