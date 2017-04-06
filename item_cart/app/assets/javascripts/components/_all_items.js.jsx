5ada7d140512ff9f05524e77e25ecb2f8deb41c4
kekoa428 <thomaskekoafarr@gmail.com>
2017-04-06 14:27:29 -0700

    creation of new item is working correctly with update in body

--- item_cart/app/assets/javascripts/components/_all_items.js.jsx
+++ item_cart/app/assets/javascripts/components/_all_items.js.jsx
@@ -1,17 +1,9 @@
 
 var AllItems = React.createClass({
-  getInitialState() {
-    return { items: [] }
-  },
-
-  componentDidMount() {
-    $.getJSON('/api/v1/items.json', (response) => { this.setState({ items: response }) });
-  },
-
   render() {
-    var items = this.state.items.map((item) => {
+    var items = this.props.items.map((item) => {
       return (
-        <div>
+        <div key={item.id}>
           <h3>{item.name}</h3>
           <p>{item.description}</p>
         </div>
